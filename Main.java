e83e11f982f2731b9b56f540ad18f4b1864cfe6f
Domeaeng <dome37688@gmail.com>
2025-09-21 17:58:46 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("HTV003", "Family Tour Bus", "ComfortRide", 2024, "Global Travels", 60000.00);
+
+        vehicle.displayInfo();
+    }
+}
